8df64f540ca556328b4db7e51440016ca8a6fdd0
Yatharth Shah <yatharhshah00@gmail.com>
2024-07-27 02:18:00 -0400

    Just change some text

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,9 +2,12 @@ function About() {
   return (
     <div>
       <h1 className="text-6xl mb-4">Github Finder</h1>
-      <h1>About This App</h1>
-      <p>App to search Github users</p>
-      <p>Version: 1.0.0</p>
+      <h2 className="text-3xl mb-4">About This App</h2>
+      <ul className="list-disc list-inside space-y-2 ml-5">
+        <li>Retrieved user data through an API and displayed profile pictures and usernames of all relevant users.</li>
+        <li>Allowed users to click on individual profiles to see detailed information including repositories, followers, following & etc.</li>
+        <li>Enabled navigation to GitHub link of user.</li>
+      </ul>
     </div>
   );
 }
